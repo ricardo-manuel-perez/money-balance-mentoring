d36792ed00e906501c3ba6c300ac0053c631d50c
Raul Marvan Medina <raul.marvan@wizeline.com>
2022-03-15 15:24:53 -0600

    Added firebase service

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-undef */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
@@ -14,24 +12,8 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
-};
-
+}
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
-
-
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-
-export const auth = getAuth();
-
-export const signInWithGoogle = () => { 
-  return signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    }).catch((error) => {
-      console.log(error)
-    });
-}
 
 export default firebase;
