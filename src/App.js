7f6ea6e9a64c81da023540fda1952ba8511cef50
Raul Marvan Medina <raul.marvan@wizeline.com>
2022-03-15 15:24:13 -0600

    Added Auth context provider to wrapp app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import HomePage from './containers/pages/HomePage/homePage';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Routes from './routes/Routes';
+import { AuthProvider } from './utils/Auth/use-auth';
+import firebase from './services/firebase';
 
 const theme = createTheme();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <HomePage/>
-      </div>
+      <AuthProvider firebase={firebase}>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
