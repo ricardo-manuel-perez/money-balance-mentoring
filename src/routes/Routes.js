489ce387075f9bf06d4f9919b24cc266f2245604
Ricardo Perez <91570123+ricardo-manuel-perez@users.noreply.github.com>
2022-03-18 16:52:11 -0600

    add protected route component

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -9,32 +9,44 @@ import Login from '../components/Login/login';
 import { useAuth } from '../utils/Auth/use-auth';
 import Home from '../components/Home/home';
 
+const ProtectedRoute = ({ route, auth }) => {
+  return (
+    <Route path={route.path} render={(props, location) => {
+      if (auth.data && !auth.error && !auth.isLoading) {
+        return <route.component {...props} />
+      } else if (!isLoading && !auth.data || (error && !isLoading)) {
+        return <Redirect to={{
+          pathname: '/login',
+          state: { from: location }
+        }}></Redirect>
+      }
+    }} />
+  )
+}
+
 const Routes = () => {
+  // auth = {isLoading, error, data:{user}}
   const auth = useAuth();
   const userLoggedIn = auth.user;
   return (
     <Router>
       <Switch>
-        <Route
-          path={ '/login' }
-          exact={ true }
-          render={ () => userLoggedIn ? <Redirect to={ { pathname: '/home' } }/> : <Login /> }
+        {/* <Route
+          path={'/login'}
+          exact={true}
+          render={() => auth.data ? <Redirect to={{ pathname: '/home' }} /> : <Login />}
+        /> */}
+        <ProtectedRoute
+          path={'/home'}
+          auth={auth}
+          render={Home}
         />
         <Route
-          path={ '/home' }
-          exact={ true }
-          render={ () => userLoggedIn ? <Home/> : <Redirect to={ { pathname: '/login' } }/> }
+          path={'/'}
+          exact={true}
+          render={() => auth.data ? <Home /> : <Redirect to={{ pathname: '/login' }} />}
         />
-        <Route
-          path={ '/' }
-          exact={ true }
-          render={ () => userLoggedIn ? <Home/>  : <Redirect to={ { pathname: '/login' } }/> }
-        />
-        <Route
-          path={ '*' }
-          exact={ true }
-          render={ () => userLoggedIn ? <Home/>  : <Redirect to={ { pathname: '/login' } }/> }
-        />
+        <Route component={() => (<h1>404 Not found</h1>)}/>
       </Switch>
     </Router>
   )
