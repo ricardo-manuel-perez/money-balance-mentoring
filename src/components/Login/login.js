154e25b041740e9d8798e54a3d20d34bb15592ae
Ricardo Perez <91570123+ricardo-manuel-perez@users.noreply.github.com>
2022-03-18 16:51:34 -0600

    Add google authentication function

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -8,62 +8,66 @@ import Grid from '@mui/material/Grid';
 import { ReactComponent as LockOutlinedIcon } from '../../assets/svg/money-icon.svg';
 import Typography from '@mui/material/Typography';
 import Banner from '../../assets/images/login-banner.png';
-import { useAuth } from '../../utils/Auth/use-auth';
-
+import { loginWithGoogle } from '../../utils/Auth/google-authentication';
 export default function Login() {
-  const { GoogleLogin } = useAuth();
+
+  const history = useHistory();
+
+  const onLoginSuccess = (data) => {
+    history.push('/home');
+  }
 
   const handleLogin = (e) => {
     e.preventDefault();
-    GoogleLogin();
+    loginWithGoogle(onLoginSuccess)
   }
 
   return (
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: `url(${Banner})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
           sx={{
-            backgroundImage: `url(${Banner})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 2, bgcolor: 'white', border: '1px solid black' }}>
-              <LockOutlinedIcon width={ 30 } /> 
-            </Avatar> 
-            <Typography component="h1" variant="h2">
-              Money Balance
-            </Typography>
-            <Box sx={{ mt: 1 }}>
-              <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                onClick={handleLogin}
-              >
-                Sign in with google
-              </Button>
-            </Box>
+        >
+          <Avatar sx={{ m: 2, bgcolor: 'white', border: '1px solid black' }}>
+            <LockOutlinedIcon width={30} />
+          </Avatar>
+          <Typography component="h1" variant="h2">
+            Money Balance
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Sign in with google
+            </Button>
           </Box>
-        </Grid>
+        </Box>
       </Grid>
+    </Grid>
   );
 }
---
+++ src/utils/Auth/google-authentication.js
@@ -0,0 +1,14 @@
+import { signInWithPopup, GoogleAuthProvider, browserSessionPersistence, setPersistence } from 'firebase/auth';
+import { auth, googleAuthProvider } from '../../services/firebase';
+
+
+export async function loginWithGoogle({ loginSuccessCallback }) {
+    try {
+        // override google authentication default place to save session data
+        await setPersistence(auth, browserSessionPersistence);
+        const googleSession = await signInWithPopup(auth, googleAuthProvider);
+        loginSuccessCallback(googleSession);
+    } catch (error) {
+        console.error(error)
+    }
+}
