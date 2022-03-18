ab91b0a8469500bf28c9721fb272e7e26e36ea65
Ricardo Perez <91570123+ricardo-manuel-perez@users.noreply.github.com>
2022-03-18 16:51:11 -0600

    Remove firebase from context

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Routes from './routes/Routes';
 import { AuthProvider } from './utils/Auth/use-auth';
-import firebase from './services/firebase';
 
 const theme = createTheme();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <AuthProvider firebase={firebase}>
+      <AuthProvider>
         <Routes />
       </AuthProvider>
     </ThemeProvider>
