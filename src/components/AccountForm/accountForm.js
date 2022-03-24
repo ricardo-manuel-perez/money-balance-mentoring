d3d1686bfc3c4e793799b0a2cd8a1c73b118447f
Raul Marvan Medina <raul.marvan@wizeline.com>
2022-03-23 18:19:02 -0600

    Added Account form using Base form to Add/Edit accounts

---
+++ src/components/AccountForm/accountForm.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import BaseForm from '../BaseForm/baseForm';
+import PropTypes from 'prop-types';
+
+const AccountForm = ({ accountId = null }) => {
+    const [formAccount, setFormAccount] = useState({
+        name: '',
+        amount: 0,
+        type: 1
+    });
+
+    const updateFormAccount = (newFormAccount) => {
+        setFormAccount(newFormAccount);
+    }
+
+    const isEdit = accountId !== null;
+
+    const sendRequestToUpdate = () => {
+        alert('update account');
+    }
+
+    const onSubmit = (data) => {
+        if (isEdit)
+            sendRequestToUpdate();
+        setFormAccount(data);
+    };
+
+    return (
+        <div className="App">
+            <h1>{isEdit ? 'Edit Account' : 'Add Account'}</h1>
+            <BaseForm initialValues={{
+                    name: '',
+                    amount: 0,
+                    type: 1
+                }}
+                onSubmit={onSubmit}
+                submitLabel={ isEdit ? 'Actualizar cuenta' : 'Agregar cuenta'}
+                formInputs={[
+                    {
+                        type: 'text',
+                        label: 'Nombre de la cuenta',
+                        name: 'name',
+                        onChange: (e) => { updateFormAccount({
+                            ...formAccount,
+                            name: e.target.value
+                        })},
+                        //onBlur: () => { },
+                        value: formAccount.name
+                    },
+                    {
+                        type: 'number',
+                        label: 'Monto inicial',
+                        name: 'amount',
+                        onChange: (e) => { updateFormAccount({
+                            ...formAccount,
+                            amount: e.target.value
+                        })},
+                        onBlur: () => { },
+                        value: formAccount.amount
+                    },
+                    {
+                        type: 'select',
+                        label: 'Tipo de cuenta',
+                        name: 'type',
+                        onChange: (e) => { updateFormAccount({
+                            ...formAccount,
+                            type: e.target.value
+                        })},
+                        onBlur: () => { },
+                        value: formAccount.type,
+                        helperText: "Selecciona un tipo de cuenta",
+                        options: [
+                            {
+                                value: 1,
+                                label: 'Type one',
+                            },
+                            {
+                                value: 2,
+                                label: 'Type two',
+                            }
+                        ]
+                    }
+                ]} />
+            <h1>Account to add</h1>
+            <code>{JSON.stringify(formAccount)}</code>
+        </div>
+    );
+}
+
+AccountForm.propTypes = {
+    accountId: PropTypes.string
+}
+
+export default AccountForm;
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,57 +1,20 @@
 import React, { useState } from 'react';
 import './home.css';
 import Navbar from '../Navbar/navbar';
-import { Card, CardContent } from '@mui/material';
+import { Card, CardContent, Modal } from '@mui/material';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import AccountForm from '../AccountForm/accountForm';
 
 const Home = () => {
-  const [accounts] = useState([
-    {
-      name: 'Account 1',
-      amount: '$ 1500,8665.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    },
-    {
-      name: 'Account 2',
-      amount: '$ 340,561.00'
-    }
-  ]);
+  const [accounts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (<>
     <Navbar />
@@ -69,7 +32,7 @@ const Home = () => {
               variant="contained"
               color="success"
               sx={{ mt: 3, mb: 2 }}
-              onClick={() => { }}
+              onClick={handleOpen}
             >
               <AddIcon />
               Agregar cuenta nueva
@@ -121,6 +84,27 @@ const Home = () => {
         })}
       </div>
     </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 800,
+        height: 'auto',
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        borderRadius: 10,
+        p: 4
+      }}>
+        <AccountForm />
+      </Box>
+    </Modal>
   </>
   );
 }
