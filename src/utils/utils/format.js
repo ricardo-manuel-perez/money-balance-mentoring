a86724d8701f02fdd558cffafdc23592f877fef7
Raul Marvan Medina <raul.marvan@wizeline.com>
2022-03-28 12:34:57 -0600

    Added currency formatter

--- src/utils/utils/format.js
+++ src/utils/utils/format.js
@@ -1,3 +1,9 @@
 export function financial(x) {
     return "$ " + Number.parseFloat(x).toFixed(2);
 }
+
+export const currencyFormatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+});
