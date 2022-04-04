2476973c4ec4460fb0007a8faced1a059f86fcfe
Raul Marvan Medina <raul.marvan@wizeline.com>
2022-04-04 16:42:25 -0500

    Added generic custom hook for entities

---
+++ src/services/Entity/entity.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { onSnapshot } from 'firebase/firestore';
+import { useAuth } from '../../utils/Auth/use-auth';
+
+export const UseGetEntity = (entityQuery) => {
+    const auth = useAuth();
+    const [entitiesState, setEntitiesState] = useState([]);
+    useEffect(() => {
+        if (auth.data && !auth.isLoading) {
+            const unsubscribe = onSnapshot(entityQuery, (querySnapshot) => {
+                const entities = [];
+                querySnapshot.forEach((doc) => {
+                    entities.push({ ...doc.data(), id: doc.id });
+                });
+                setEntitiesState(entities);
+            });
+            return () => unsubscribe();
+        }
+    }, [entityQuery]);
+    return entitiesState;
+}
