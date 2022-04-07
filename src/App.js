4d2324f71a4cd3eb2d4d886c336dca771ae58d9b
Raul Marvan Medina <raul.marvan@wizeline.com>
2022-04-07 10:56:46 -0500

    Added localization provider for datepicker input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React, { Suspense } from "react";
+import React, { useState, Suspense, useEffect } from "react";
 import "./App.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Routes from "./routes/Routes";
 import { AuthProvider } from "./utils/Auth/use-auth";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-
-const theme = createTheme();
+import ThemeSwitcher from "./components/ThemeSwitcher/themeSwitcher";
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 function App() {
   const queryClient = new QueryClient();
+  const [mode, setMode] = useState(() => {
+    let localMode = localStorage.getItem('mode') || 'light'
+    return localMode;
+  });
+
+  const [theme, setTheme] = useState(
+    createTheme({
+      palette: {
+        mode: mode
+      },
+    })
+  );
+
+  useEffect(() => {
+      setTheme(createTheme({
+        palette: {
+          mode: mode
+        }
+      }))
+  }, [mode, setMode]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <AuthProvider>
-          <Suspense fallback={<div>Loading ...</div>}>
-            <Routes />
-          </Suspense>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Suspense fallback={<div>Loading ...</div>}>
+              <ThemeSwitcher mode={mode} setMode={setMode} />
+              <Routes />
+            </Suspense>
+
+          </LocalizationProvider>
         </AuthProvider>
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
